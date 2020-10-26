480a47a56e8e5d27504755b21f7391aab6bb13b0
Yeh Cheng Liang <showtime201005@gmail.com>
2020-10-26 17:05:29 +0800

    revert to 02cc711dc43cb867c31176f43c686082f35e6bba

--- test-git.py
+++ test-git.py
@@ -1,7 +1,3 @@
 print('test git')
 print('if changes in staging area, what happen?')
-<<<<<<< HEAD
 print('you need to re-add')
-=======
-print('you need to re-add')
->>>>>>> parent of 02cc711 (test git revert and add message(git revert))
